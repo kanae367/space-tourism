010da5dc6cc35be43b3551fe49b839777f4fb53c
”Kodoku” <kodokunawa@gmail.com>
2023-03-20 15:51:16 +0300

    Добавлены анимации, новые бэкграунды

--- source/js/main.js
+++ source/js/main.js
@@ -29,10 +29,9 @@ const navClickHandler = function() {
 
     navElement.parent().children().removeClass(`${navElement.parent().attr('class')}-item_active`);
     navElement.addClass(`${navElement.attr('class')}_active`);
+    if(navElement.parent().is('.popup__list')) popup.removeClass('popup_active');
     
     setTimeout(function(){
-        if(navElement.parent().is('.popup__list')) popup.removeClass('popup_active');
-
         const activeClass = navElement.find('.nav-link').attr('data-page') + '-active';
         contentContainer.attr('class', `container ${activeClass}`);
     }, 400)
@@ -42,16 +41,15 @@ popupNavElements.on('click', navClickHandler);
 headerNavElements.on('click', navClickHandler);
 
 const destinationNavElements = $(".destination__list-item *");
-const planetImageElement = $(".destination__picture > img");
+const planetImageElement = $(".destination__picture");
 const planetStatElements = $(".info__stats-accent");
 const planetTitleElement = $(".info__title");
 const planetTextElement = $(".info__text");
 
 //замена текстового контента с анимацией
 const changeText = function(element, newText){
-    element.addClass('swap-animation')
-    .delay(400)
-    .queue('fx', function(){$(this).text(newText).removeClass('swap-animation').dequeue('fx');})
+    element.fadeOut()
+    .queue('fx', function(){$(this).text(newText).fadeIn().dequeue('fx');})
 };
 
 const destinationNavClickHandler = function(){
@@ -61,9 +59,8 @@ const destinationNavClickHandler = function(){
     destinationNavElements.removeClass('nav-link_active');
     $(this).addClass('nav-link_active');
 
-    planetImageElement.addClass('swap-animation')
-        .delay(400)
-        .queue('fx', function(){$(this).attr('src', `source/images/destination/${planet.image}`).removeClass('swap-animation').dequeue('fx');})
+    planetImageElement.fadeOut()
+        .queue('fx', function(){$(this).find('img').attr('src', `source/images/destination/${planet.image}`).parent().fadeIn().dequeue('fx');})
     changeText(planetTitleElement, planet.name);
     changeText(planetTextElement, planet.text);
     changeText(planetStatElements.eq(0), planet.facts.distance);
@@ -85,9 +82,8 @@ const crewNavClickHandler = function(){
     crewNavElements.removeClass('crew__nav-link_active');
     $(this).addClass('crew__nav-link_active');
 
-    memberPhotoElement.addClass('swap-animation')
-    .delay(400)
-    .queue('fx', function(){$(this).attr('src', `source/images/crew/${member.photo}`).removeClass('swap-animation').dequeue('fx');});
+    memberPhotoElement.fadeOut()
+    .queue('fx', function(){$(this).attr('src', `source/images/crew/${member.photo}`).fadeIn().dequeue('fx');});
     changeText(memberNameElement, member.name);
     changeText(memberPositionElement, member.position);
     changeText(memberInfoElement, member.info);
@@ -107,9 +103,8 @@ const techNavClickHandler = function(){
     techNavElements.removeClass('technology__nav-btn_active');
     $(this).addClass('technology__nav-btn_active');
 
-    techImage.addClass('swap-animation')
-        .delay(500)
-        .queue('fx', function(){$(this).attr('class',  `technology__image technology__image_${tech.id}`).removeClass('swap-animation').dequeue('fx');})
+    techImage.fadeOut()
+        .queue('fx', function(){$(this).attr('class',  `technology__image technology__image_${tech.id}`).fadeIn().dequeue('fx');})
     changeText(techTitle, tech.title);
     changeText(techInfo, tech.text);
 };
@@ -129,5 +124,35 @@ const homeBtnClickHandler = function(){
 };
 
 homeBtn.on('click', homeBtnClickHandler);
+
+if($(window).innerWidth() > 1400){
+    setInterval(function(){
+        let randomDirection = ['', '', '', '', '', ''];
+        for(let i = 1; i <= randomDirection.length; i++){
+            let random = Math.round(Math.random());
+            if(random === 0){
+                randomDirection[i] = '-';
+            }
+        }
+    
+        let randomNumberX = Math.round(Math.random() * 6);
+        let randomNumberY = Math.round(Math.random() * 6);
+    
+        document.documentElement.style.setProperty('--moveX', `${randomDirection[0]}${randomNumberX}px`);
+        document.documentElement.style.setProperty('--moveY', `${randomDirection[1]}${randomNumberY}px`);
+    
+        let randomNumberXp = Math.round(Math.random() * 6);
+        let randomNumberYp = Math.round(Math.random() * 6);
+    
+        document.documentElement.style.setProperty('--moveXp', `${randomDirection[2]}${randomNumberXp}px`);
+        document.documentElement.style.setProperty('--moveYp', `${randomDirection[3]}${randomNumberYp}px`);
+    
+        let randomNumberXt = Math.round(Math.random() * 6);
+        let randomNumberYt = Math.round(Math.random() * 6);
+    
+        document.documentElement.style.setProperty('--moveXt', `${randomDirection[4]}${randomNumberXt}px`);
+        document.documentElement.style.setProperty('--moveYt', `${randomDirection[5]}${randomNumberYt}px`);
+    }, 3900)
+}
 
 });
