2dc9dc4da042070270122260d33d82104fd97b1c
”Kodoku” <kodokunawa@gmail.com>
2023-08-28 13:32:02 +0300

    Улучшена структура homeButton.js

--- source/js/components/pages/homeButton.js
+++ source/js/components/pages/homeButton.js
@@ -1,17 +1,18 @@
 import { navClickHandler } from "../navigationClick.js";
 
-export const setHomeEventListeners = () => {
-    const headerNavElements = $(".header__nav-list-item");
+const headerNavElements = $(".header__nav-list-item");
 
-    $('.home__button').on('click', () => {
-        headerNavElements.removeClass('header__nav-list-item_active');
-        headerNavElements.eq(1).addClass('header__nav-list-item_active');
-        $('.content-overlay').fadeIn().fadeOut();
+const handleHomeButtonClick = () => {
+    headerNavElements.removeClass('header__nav-list-item_active');
+    headerNavElements.eq(1).addClass('header__nav-list-item_active');
+    $('.content-overlay').fadeIn().fadeOut();
     
-        setTimeout(function(){
-            $(".container").attr('class', 'container destination-active');
-        }, 400);
-    });
+    setTimeout(function(){
+        $(".container").attr('class', 'container destination-active');
+    }, 400);
+}
 
+export const setHomeEventListeners = () => {
+    $('.home__button').on('click', handleHomeButtonClick);
     headerNavElements.on('click', navClickHandler);
 }
